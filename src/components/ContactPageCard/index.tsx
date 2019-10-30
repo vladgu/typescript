be7fdd1e995287cd9997c2b90dc8909528b32f9e
Vladyslav-Gulyaev <vlad040494@gmail.com>
2019-10-30 17:20:02 +0200

    added typescript: part 2

--- src/components/ContactPageCard/index.tsx
+++ src/components/ContactPageCard/index.tsx
@@ -14,7 +14,6 @@ const PageCard = ({ login, avatar_url, html_url, ...rest }: PageCardTypes) => {
   const goToCard = () => {
     rest.history.push("/contacts/" + login.toLowerCase());
   };
-  // console.log(rest);
   return (
     <div className="page-card" onClick={goToCard}>
       <h3>{login}</h3>
